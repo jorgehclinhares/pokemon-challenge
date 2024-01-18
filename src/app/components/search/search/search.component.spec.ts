a583202842c75bbce8aa918f255c2d30f31eec85
Jorge Linhares <jorgehclinhares@gmail.com>
2024-01-18 11:12:03 -0300

    test: add test for search component

--- src/app/components/search/search/search.component.spec.ts
+++ src/app/components/search/search/search.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { PokemonServiceModule } from '../../../services/pokemon/pokemon.service.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -8,10 +11,9 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchComponent]
-    })
-    .compileComponents();
-    
+      imports: [SearchComponent, ReactiveFormsModule, PokemonServiceModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +21,41 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit search', () => {
+    const spyEmitSearch = jest.spyOn(component['pokemonService'], 'emitSearch');
+    component.emitSearch('pikachu');
+    expect(spyEmitSearch).toHaveBeenCalledWith('pikachu');
+  });
+
+  it('should emit search after change input value', () => {
+    const spyEmitSearch = jest.spyOn(component['pokemonService'], 'emitSearch');
+
+    const searchInput = fixture.debugElement.query(
+      By.css('#searchInput'),
+    ).nativeElement;
+
+    searchInput.value = 'snorlax';
+    searchInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(spyEmitSearch).toHaveBeenCalledWith('snorlax');
+    });
+  });
+
+  it('should dispatch destroy for subscribes on destroy component', () => {
+    const spySearchMonitDestroyNext = jest.spyOn(
+      component['searchMonitDestroy'],
+      'next',
+    );
+    const spySearchMonitDestroyComplete = jest.spyOn(
+      component['searchMonitDestroy'],
+      'complete',
+    );
+    component.ngOnDestroy();
+    expect(spySearchMonitDestroyNext).toHaveBeenCalled();
+    expect(spySearchMonitDestroyComplete).toHaveBeenCalled();
   });
 });
