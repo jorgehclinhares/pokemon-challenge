491e24d27011b9dfe5da838f84114d22fa873358
Jorge Linhares <jorgehclinhares@gmail.com>
2024-01-18 09:19:54 -0300

    test: like component tests

--- src/app/components/like/like/like.component.spec.ts
+++ src/app/components/like/like/like.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LikeComponent } from './like.component';
+import { By } from '@angular/platform-browser';
 
 describe('LikeComponent', () => {
   let component: LikeComponent;
@@ -8,10 +9,9 @@ describe('LikeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LikeComponent]
-    })
-    .compileComponents();
-    
+      imports: [LikeComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LikeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,31 @@ describe('LikeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle like and emit value on likeOrUnLike function', () => {
+    component.favorited = false;
+    const spy = jest.spyOn(component.onLike, 'emit');
+    component.likeOrUnlike();
+    expect(component.favorited).toBeTruthy();
+    expect(spy).toHaveBeenCalledWith(true);
+  });
+
+  it('should render like button on favorited is true', () => {
+    component.favorited = true;
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(
+      By.css('.favorited'),
+    )?.nativeElement;
+    expect(button).toBeTruthy();
+  });
+
+  it('should render unlike button on favorited is false', () => {
+    component.favorited = false;
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(
+      By.css('.favorited'),
+    )?.nativeElement;
+    expect(button).toBeUndefined();
   });
 });
